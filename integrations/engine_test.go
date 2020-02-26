d6df2e8a8c57711da5f48ebe375c1d107c61e587
Lunny Xiao <xiaolunwen@gmail.com>
2020-02-26 10:04:04 +0800

    move goracle to godror

--- integrations/engine_test.go
+++ integrations/engine_test.go
@@ -16,11 +16,13 @@ import (
 
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/godror/godror"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/stretchr/testify/assert"
 	_ "github.com/ziutek/mymysql/godrv"
 	_ "modernc.org/sqlite"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPing(t *testing.T) {
